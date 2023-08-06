bf41285b9df8661954d7738baf2f7ae69dbaefb9
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2023-08-06 01:00:55 -0700

    Update serviceDay.php
    Add new sign up

--- html/pages/service/serviceDay.php
+++ html/pages/service/serviceDay.php
@@ -12,13 +12,13 @@ startSession();
     <div id="main">
       <div class="col">
 	<h1>Service Day</h1>
-        <p>Service Day will be hosted by APO on October 1st to connect RPI students 
+        <p>Service Day will be hosted by APO on September 30th to connect RPI students 
 	with organizations in the community to help make a difference. This will be our 
 	third Service Day and first after the pandemic, we are planning to do service at the 
-	Regional Food Bank, Frear Park, Oakwood Community Center, Sanctuary for Independent Media, Troy Bike Rescue, 
-	and a campus clean on campus and at other nearby parks. Food and transportation will be 
-	provided to all participants. Sign up below and view our poster below the sign up. </p>
-	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSewZKQlw7weGQ6mXMzGCZaH2pJntHFjPU73WbtlirIuqQToTA/viewform?embedded=true" width="640" height="2800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+	Regional Food Bank, Frear Park, Northern Rivers, Sanctuary for Independent Media, Mohawk Hudson Humane Society, 
+	and card-making on campus for soldiers. Food and transportation will be 
+	provided to all participants. Sign up below and view our poster below the signup. </p>
+	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSflkNvRu9wzPXeKz0lJ-DVntkN_NWg2eiyX_BoWu2scHcYOKQ/viewform?embedded=true" width="640" height="2800" frameborder="0" marginheight="0" marginwidth="0">Loadingâ€¦</iframe>
       </div>
     </div>
   </body>
